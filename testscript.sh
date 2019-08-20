117a84ca0f180fcaa6e0a60041c3621aadfeb536
joshua.parker@circulation.com <joshua.parker@circulation.com>
2019-08-20 09:31:42 -0400

    new text`

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo ":bUILD MYSELF UP retire at 35"
+echo ":bUILD MYSELF UP retire at 35 I WILL SUCCEED !! "
 echo "this was run automagically"
