31602ceb28d46f59cf049d326086aa222b271f56
joshua.parker@circulation.com <joshua.parker@circulation.com>
2019-08-21 12:59:36 -0400

    Fixed webhooks

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,5 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo ":bUILD MYSELF UP retire at 35 I WILL SUCCEED !! "
 echo "this was run automagically"
-echo "this was run With linux Far78997 out !!"
+echo "this was run With linux far out !!"
+echo "I fixed the webhooks, the server address changes each day"
