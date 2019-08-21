8ab5c0e67802e8696c6e01a93a8b00d27dcd34af
joshua.parker@circulation.com <joshua.parker@circulation.com>
2019-08-21 12:52:23 -0400

    Added new line

--- testscript.sh
+++ testscript.sh
@@ -4,4 +4,4 @@ NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo ":bUILD MYSELF UP retire at 35 I WILL SUCCEED !! "
 echo "this was run automagically"
-echo "this was run With linux Far out !!"
+echo "this was run With linux Far78997 out !!"
